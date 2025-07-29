2561b9c40f48e99ed5763df9f4d82572ee3a3cfd
Shino-Song <shinovtofficial@gmail.com>
2025-07-30 04:10:37 +0900

    added an interactive CLI promt with main.py and gave it some inputs to utilize the balances.py logic.

---
+++ main.py
@@ -0,0 +1,29 @@
+from core.balances import *
+
+print("Hello, this is the Song Financial Planner. I hope you are having a great day!")
+print("To start, I will ask you a couple simple questions.")
+while True:
+    est_input = input("First, what is your estimated balance?\nEstimate: ")
+    est_yn = input(f"Wonderful! Your estimated balance is {est_input}, is that correct? (Y/N): ")
+    if est_yn.lower() == "y":
+        break
+    else:
+        print("Let's try that again.\n")
+print("Perfect! Now for the next question.")
+while True:
+    act_input = input("Next, what is your actual balance?\nActual: ")
+    act_yn = input(f"Amazing! Your actual balance is {act_input}, is that correct?\n (Y/N): ")
+    if act_yn.lower() == "y":
+        break
+    else:
+        print("Let's try again.\n")
+
+act_transfer = actual(act_input)
+est_transfer = estimate(est_input)
+
+report_result = report(act_transfer, est_transfer)
+
+print("\nThank you for using the Song Financial Planner input test!")
+print(f"Your estimated balance was {est_transfer:.2f} and your actual balance was {act_transfer:.2f}.")
+print(f"The following is your balance report.\n{report_result}")
+exit()
