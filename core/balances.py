54c2ce0dcb084178d79230c6052dea14e1dcc974
Shino-Song <shinovtofficial@gmail.com>
2025-08-04 19:22:14 +0900

    fixing file dupe

--- core/balances.py
+++ core/balances.py
@@ -1,8 +1,10 @@
 def actual(act):
-    return float(act)
+    act_balance = act
+    return act_balance
 
 def estimate(est):
-    return float(est)
+    est_balance = est
+    return est_balance
 
 def act_est_compare(actual_bal, estimate_bal):
     difference = actual_bal - estimate_bal
@@ -11,6 +13,12 @@ def act_est_compare(actual_bal, estimate_bal):
 def report(actual_bal, estimate_bal):
     difference = act_est_compare(actual_bal, estimate_bal)
     if difference != 0:
-        return (f"balance not accurate. Please review accounting! Difference is {difference:.2f}")
+        return (f"balance not accurate. Please review accounting! Difference is {difference}")
     else:
         return "All is well in the world."
+
+act_input = actual(10)
+est_input = estimate(100)
+
+report_result = report(act_input, est_input)
+print(report_result)
