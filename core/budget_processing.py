be68f5cc1d7e8ef4b9d537bcb5f06007274da74c
Shino-Song <shinovtofficial@gmail.com>
2025-09-25 21:03:15 +0900

    budget_processing v0.1

--- core/budget_processing.py
+++ core/budget_processing.py
@@ -1,4 +1,13 @@
 class budget:
-    def __init__(self)
+    def __init__(self, estimate, actual)
+        self.__estimate = estimate
+        self.__actual = actual
+        self.__difference = 0
 
+class expense(budget):
+    def __init__(self)
+        super().__init__(self)
     
+    def diff_calc():
+        self.__difference = self.__actual - self.__estimate
+        
